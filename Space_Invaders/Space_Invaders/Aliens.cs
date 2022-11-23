0f2f7bf5e3eb0df4e6bf5473b83f7146b23e36b1
ahmadalasfar <111990740+ahmadalasfar@users.noreply.github.com>
2022-11-23 15:46:04 +0100

    Update Aliens.cs

--- Space_Invaders/Space_Invaders/Aliens.cs
+++ Space_Invaders/Space_Invaders/Aliens.cs
@@ -9,24 +9,56 @@ namespace Space_Invaders
     public class Aliens
     {
         private int _y;
+        private int _x;
+
+
+
+        private static string[] _symbol = new string[5]                // tableau du symbole
+        {
+               "  ▀▄   ▄▀  ",
+               " ▄█▀███▀█▄ ",
+               "█▀███████▀█",
+               "█░█▀▀▀▀▀█░█",
+               "   ▀▀░▀▀   "
+        };
+
+
+        private static string[] _noSymbol = new string[5]                // tableau du symbole
+        {
+               "           ",
+               "           ",
+               "           ",
+               "           ",
+               "           "
+        };
 
         /// <summary>
         /// Default constroctor
         /// </summary>
-        public Aliens()
+        public Aliens(int X, int Y)
         {
-
-           
+            _x = X;
+            _y = Y;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int Y
         {
             get { return _y; }
             set { _y = value; }
         }
 
-
-
-
+        
     }
 }
